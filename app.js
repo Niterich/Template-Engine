d628aed33bf988ec2dc9c0ed96f02cc27a4ff25e
Niterich <52295078+Niterich@users.noreply.github.com>
2020-02-04 09:48:37 -0700

    working on looping back through the questions for more team members

--- app.js
+++ app.js
@@ -5,78 +5,132 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-let man;
+let manager;
 let eng;
 let int;
+let teamMembersArray = [];
 
-inquirer
-    .prompt([
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is your name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "What is your ID number?",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "What is your email address?",
+                name: "email"
+            },
+            {
+                type: "list",
+                message: "Please select your role.",
+                name: "title",
+                choices: ["Manager", "Engineer", "Intern"]
+            }
+        ])
+        .then(res => {
+            const info = res;
+            if (info.title === "Manager") {
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: "Please enter your office number.",
+                            name: "officeNumber"
+                        }
+                    ])
+                    .then(res => {
+                        manager = new Manager(
+                            info.name,
+                            info.id,
+                            info.email,
+                            res.officeNumber
+                        );
+                        console.log(manager);
+                        teamMembersArray.push(manager);
+                        wouldContinue();
+                    });
+            }
+            if (info.title === "Engineer") {
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: "Please enter your Github username",
+                            name: "github"
+                        }
+                    ])
+                    .then(res => {
+                        eng = new Engineer(
+                            info.name,
+                            info.id,
+                            info.email,
+                            res.github
+                        );
+                        console.log(eng);
+                        teamMembersArray.push(eng);
+                        wouldContinue();
+                    });
+            }
+            if (info.title === "Intern") {
+                inquirer
+                    .prompt([
+                        {
+                            type: "input",
+                            message: "Which college do/did you attend?",
+                            name: "school"
+                        }
+                    ])
+                    .then(res => {
+                        int = new Intern(
+                            info.name,
+                            info.id,
+                            info.email,
+                            res.school
+                        );
+                        console.log(int);
+                        teamMembersArray.push(int);
+                        wouldContinue();
+                    });
+            }
+        });
+}
+
+function wouldContinue(){
+    inquirer.prompt([
         {
-            type: "input",
-            message: "What is your name?",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "What is your ID number?",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "What is your email address?",
-            name: "email"
-        },
-        {
-            type: "list",
-            message: "Please select your role.",
-            name: "title",
-            choices: ["Manager", "Engineer", "Intern"]
+            type: "confirm",
+            message: "Would you like to add another team member?",
+            name: "continue"
         }
-    ]).then (res => {
-        const info = res;
-        if (info.title === "Manager") {
-            inquirer.prompt([
-                {
-                    type: "input",
-                    message: "Please enter your office number.",
-                    name: "officeNumber"
-                }
-        ]).then (res => {
-            man = new Manager(info.name, info.id, info.email,res.officeNumber);
-            console.log(man);
-        })
-    };
-        if (info.title === "Engineer"){
-            inquirer.prompt([
-                {
-                    type: "input",
-                    message: "Please enter your Github username",
-                    name: "github"
-                }
-        ]).then (res => {
-            eng = new Engineer(info.name, info.id, info.email, res.github);
-            console.log(eng);
-        })
-    };
-        if (info.title === "Intern"){
-            inquirer.prompt([
-                {
-                    type: "input",
-                    message: "Which college do/did you attend?",
-                    name: "school"
-                }
-        ]).then (res => {
-            int = new Intern(info.name, info.id, info.email, res.school);
-            console.log(int);
-        })
-    };
-});
+    ]).then(res => {
+        if (res.continue) {
+            addEmployee();
+        } else {
+            buildTeam();
+        }
+    })
+}
+
+addEmployee();
 
 
-// new objects are created, how do I create multiple engineer/interns? 
-    //prompt user for how many team members of each type?
-    //how to save the variables as the this.id number or something unique.
+// new objects are created, how do I create multiple engineer/interns?
+//prompt user for how many team members of each type?
+//how to save the variables as the this.id number or something unique.
 
-    //what do do with the info? 
-        //create a single html file and have the information populated into it?
-        //create individial html files and concat somehow?
+//what do do with the info?
+//create a single html file and have the information populated into it?
+//create individial html files and concat somehow?
+
+//to-do
+    //write templates
+    //create templates
+    //build team function
